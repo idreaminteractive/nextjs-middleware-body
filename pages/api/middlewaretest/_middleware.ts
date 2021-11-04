e15641303d2d23d7e4fae05b26545958f02f6a3c
Dave Wiper <dwiper@idreaminteractive.com>
2021-11-04 09:05:46 -0400

    setup for bug display

---
+++ pages/api/middlewaretest/_middleware.ts
@@ -0,0 +1,18 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log("method", req.method); // will be post
+  console.log("bodyUsed", req.bodyUsed); // will be false
+  console.log("headers", req.headers); // shows proper content type and content length
+  console.log("body", req.body); // always null!
+
+  /***
+   *
+   * If middleware is async and I try any of the following:
+   *
+   * await res.json() // TypeError: invalid json body reason: Unexpected end of JSON input
+   * await res.text() // blank string
+   *
+   */
+  return new Response("Hello, world!");
+}
